246f7328f51320b69564afa28ec5d83e9156e24a
Sarutch Supaibulpipat <112068235+POKerPONGus@users.noreply.github.com>
2023-10-09 11:36:50 +0700

    Adding docstrings to mock-up version of the food database manager.

--- app/food_db_manager.py
+++ app/food_db_manager.py
@@ -1,4 +1,26 @@
+'''
+food_db_manager.py
+
+This is merely a mock-up of the food database manager.
+
+Functions:
+    find_food_name
+'''
+
 def find_food_name(food_name: str) -> dict:
+    '''
+    Mock-up of finding foods by name functionality
+
+    Arguments:
+        food_name:
+            String of the food name
+
+    Returns:
+        A dictionary with the food name its nutrition values.
+
+    Expections:
+        Food not found:
+    '''
     if(food_name == "hi"):
         raise Exception("Food not found")
-    return {"food": food_name}
+    return {"food": food_name, "calories": 0, "sodium": 0}
