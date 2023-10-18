c80619e1769b6a97e540d0f0e934de306456d3ae
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2023-10-18 16:39:06 +0700

    Revert "Adding a function to find meal by date in history_db_manager + minor refactorization"
    This reverts commit cf2bda6e80893413acf20995b9d52f8b443fc539.

--- test.py
+++
@@ -1,8 +0,0 @@
-import app.history_db_manager as db
-from datetime import datetime
-
-db.create_history_database()
-date = datetime.now()
-db.add_meal_to_database(date, "dinner", ["apple", "banana"])
-print(db.find_meal_date(date, "dinner"))
-print(db.find_meal_date(date, "brunch"))
