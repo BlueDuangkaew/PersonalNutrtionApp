a4bee9482afc4f139abc7ae78d4beb6a37c6dca1
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2023-10-18 19:51:23 +0700

    correcting retrieve_all_meals's output format in eating_histry.py and beautifying the code

--- app/db_manager/eating_history.py
+++ app/db_manager/eating_history.py
@@ -6,24 +6,31 @@ history_manager.py
 Functions:
     add_meal_to_database
     retrieve_all_meals
+    find_meal_date
 '''
 import sqlite3
 from datetime import datetime
 from sqlite3 import Error
 
+__author__ = "Plam, Pokpong"
+
 COLUMNS = ("id", "date", "meal_type", "foods")
 
-def row_to_dict(row: tuple) -> dict:
+def format_row(row: tuple) -> dict:
+    # This function formats the a row of data from the database
     meal = dict(zip(COLUMNS, row))
     meal["foods"] = meal["foods"].split(", ")
     return meal
 
 # Function to create the SQLite database and table
 def create_history_database():
-    attributes = ["INTEGER PRIMARY KEY AUTOINCREMENT", "DATE", "TEXT", "TEXT"]
+    attributes = ["INTEGER PRIMARY KEY AUTOINCREMENT", 
+                  "DATE", 
+                  "INTEGER", 
+                  "TEXT"]
     column_info = (", ").join([" ".join(tup) for tup in zip(COLUMNS, attributes)])
 
-    conn = sqlite3.connect('meal_history.db')
+    conn = sqlite3.connect("meal_history.db")
     cursor = conn.cursor()
     print("history init")
     cursor.execute(f"CREATE TABLE IF NOT EXISTS meals ({column_info})")
@@ -31,7 +38,7 @@ def create_history_database():
     conn.close()
 
 # Function to add a meal to the database
-def add_meal_to_database(date: datetime, meal_type, foods: list):
+def add_meal_to_database(date: datetime, meal_type: int, foods: list):
     '''
     Adds a meal to the history database
 
@@ -42,10 +49,12 @@ def add_meal_to_database(date: datetime, meal_type, foods: list):
     
     Returns:
     '''
-    conn = sqlite3.connect('meal_history.db')
+    conn = sqlite3.connect("meal_history.db")
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO meals ({', '.join(COLUMNS[1:4])}) VALUES (?, ?, ?)",
-                   (date.strftime('%Y-%m-%d'), meal_type, ', '.join(foods)))
+    cursor.execute(f'''INSERT INTO meals ({", ".join(COLUMNS[1:4])}) 
+                   VALUES ({date.strftime("%Y-%m-%d")}, 
+                           {meal_type}, 
+                           "{", ".join(foods)}")''')
     print("meal added.")
     conn.commit()
     conn.close()
@@ -57,23 +66,31 @@ def retrieve_all_meals():
     
     Returns:
     '''
-    conn = sqlite3.connect('meal_history.db')
+    conn = sqlite3.connect("meal_history.db")
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM meals')
-    meals = cursor.fetchall()
+    cursor.execute("SELECT * FROM meals")
+    meals = list(map(format_row, cursor.fetchall()))
     conn.close()
     return meals
 
-def find_meal_date(date: datetime, meal_time: str) -> list:
+def find_meal_date(date: datetime, meal_type: int) -> dict:
     '''
-    Finds and returns a meal if found.
+    Finds a meal by date and meal time and returns a meal if found.
     
+    Arguments:
+        date:
+            The date of the meal.
+        meal_type:
+            The meal time/type.
+
     Returns:
+        A dictionary conatining the info on the meal.
     '''
-    conn = sqlite3.connect('meal_history.db')
+    conn = sqlite3.connect("meal_history.db")
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM meals WHERE {COLUMNS[1]}='{date.strftime('%Y-%m-%d')}' AND {COLUMNS[2]}='{meal_time}'")
-    meals = cursor.fetchall()
+    cursor.execute(f'''SELECT * FROM meals 
+                   WHERE {COLUMNS[1]} = "{date.strftime("%Y-%m-%d")}" 
+                   AND {COLUMNS[2]} = {meal_type}''')
+    meal = format_row(cursor.fetchone())
     conn.close()
-    meals = list(map(row_to_dict, meals))
-    return meals
+    return meal
