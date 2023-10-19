be7062f12f10ef11fdf6aca8ef966d845913837f
Teetuch Thawinphrai <83281831+ItzPalmz@users.noreply.github.com>
2023-10-19 15:58:08 +0700

    Update daily.py according to pylint

--- app/report/daily.py
+++ app/report/daily.py
@@ -22,16 +22,12 @@ def generate_report_by_date(start_date, end_date):
     '''
     eating_history_conn = sqlite3.connect('eating_history.db')
     eating_history_cursor = eating_history_conn.cursor()
-    
     eating_history_cursor.execute('''
         SELECT date, food.name, servings
         FROM eating_history
         INNER JOIN food ON eating_history.food_id = food.food_id
         WHERE date BETWEEN ? AND ?
     ''', (start_date, end_date))
-    
     report = eating_history_cursor.fetchall()
-    
     eating_history_conn.close()
-    
     return report
