74d026aa0742b28cfb6704ebe7cdaba50fd88689
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2023-11-02 19:58:53 +0700

    Minor code tweaks, renaming the check_food_exist function to food_exist.

---
+++ app/db/validator/food.py
@@ -0,0 +1,35 @@
+'''
+food_validator.py
+
+This module contains functions for validating food entries
+
+Functions:
+    food_exists
+'''
+
+import db_manager.food as db
+
+__author__ = "Pokpong"
+
+def food_exists(food_name: str) -> bool:
+    """
+    Checks if a given food name exists in the food database.
+
+    Arguments:
+        food_name: str
+            food name to search
+    
+    Returns:
+        True:
+            if found
+        False:
+            if not found
+    """
+    try:
+        db.find_food_name(food_name)
+    except Exception as ex:
+        if str(ex) == "No matching data.":
+            return False
+        else:
+            print(str(ex))
+    return True
