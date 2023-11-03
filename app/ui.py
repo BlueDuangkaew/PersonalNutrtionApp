2887935f2bab8f7b7c1d732583a66e29b2d707ba
Teetuch Thawinphrai <83281831+ItzPalmz@users.noreply.github.com>
2023-11-03 19:34:48 +0700

    updates ui.py make it cleaner

--- app/ui.py
+++ app/ui.py
@@ -21,12 +21,11 @@ def enter_date():
             elif date < datetime.now() - timedelta(days=14):
                 print("The date you entered is more than two weeks old.")
             else:
-                return date.strftime('%Y-%m-%d'), date < datetime.now()
-    return date_str
-    
+                return date.strftime('%Y-%m-%d')
+
 def enter_meal_type():
     while True:
-        meal_type = input("Enter meal type (breakfast/lunch/dinner/snack): ").lower()  # Convert to lowercase for case-insensitivity
+        meal_type = input("Enter meal type (breakfast/lunch/dinner/snack): ").lower()
         if meal_type in ["breakfast", "lunch", "dinner", "snack"]:
             return meal_type
         else:
@@ -57,52 +56,37 @@ def ask_date_range():
 
         if start_date > end_date:
             print("The start date should be before the end date.")
-    return start_date, end_date
+            continue
+        return start_date, end_date
 
 def ask_nutrition_type():
     while True:
-    # Ask the user to input a nutrition type (calories, sodium, or sugar)
         nutrition_type = input("Enter a nutrition type (calories, sodium, sugar): ").lower()
-
-    # Check if the input is valid
         if nutrition_type not in ["calories", "sodium", "sugar"]:
             print("Invalid nutrition type. Please enter calories, sodium, or sugar.")
-            return
-
-    # Ask the user to input the limit for the chosen nutrition type
-        max_value = input(f"Enter the limit for {nutrition_type}: ")
-    return nutrition_type, max_value
+        else:
+            max_value = input(f"Enter the limit for {nutrition_type}: ")
+            return nutrition_type, max_value
 
 def report_menu():
-
     while True:
         print("\nReport Menu")
         print("1. Daily report")
         print("2. Goal report")
         print("3. Back to main menu")
-
-        # Prompt the user to select an option
+        
         choice = input("Select an option (1/2/3): ")
 
-        # Check the user's choice and take appropriate actions
         if choice == '1':
-            ask_date_range()
-            generate_report_by_date(start_date, end_date) 
+            start_date, end_date = ask_date_range()
             print("Generating reports from a date range.")
-            #create function to asl for date range and check if the database is empty
-            #ask for date range
-            #is the database empty?
-            # Call the function to generate a daily report
+            # Call the function to generate a daily report using start_date and end_date
         elif choice == '2':
-            #create function to ask for nutrition type and check if the database is empty?
-            #is the database empty?
-            #ask for nutrition type
-            ask_nutrition_type()
-            generate_report_by_goal()
+            nutrition_type, max_value = ask_nutrition_type()
             print("Generating reports from the user's goal.")
-              # Call the function to generate a goal report
+            # Call the function to generate a goal report using nutrition_type and max_value
         elif choice == '3':
-            break  # Return to the main menu
+            break
         else:
             print("Invalid choice. Please select 1, 2, or 3.")
 
@@ -115,18 +99,15 @@ if __name__ == "__main__":
 
         choice = input("Select an option (1/2/3): ")
 
-        # Check the user's choice and take appropriate actions
         if choice == '1':
-
-            enter_date()
-            #create input meal type function and check for validity
-            enter_meal_type()
-            #create function to ask for food input
-            enter_food_items()
+            date = enter_date()
+            meal_type = enter_meal_type()
+            foods = enter_food_items()
+            # Process the entered meal data
         elif choice == '2':
-            report_menu()  # Call the function to display the report menu
+            report_menu()
         elif choice == '3':
             print("Exit. Have a great day!")
-            break  # Exit the program
+            break
         else:
             print("Invalid choice. Please select 1, 2, or 3.")
